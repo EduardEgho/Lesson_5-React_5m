5d17acd02980ac1f44d8bb6551f7045313a591d0
EduardEgcho <egiazaryan-eduard@example.com>
2024-06-04 16:25:24 +0300

    готово

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const NavBar = () => {
+    const activeLinkClass = ({ isActive }) => {
+        return isActive ? 'red' : ''
+    }
+
+    const cartProducts = useSelector((state) => state.cart.products)
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/" className={activeLinkClass}>
+                        Products
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/basket" className={activeLinkClass}>
+                        basket {cartProducts.length}
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
